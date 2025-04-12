1402bf00ff9a961fcd64f33daf1c52e197a9ab1a
Yecsider <jyrios@ucundinamarca.edu.co>
2025-04-12 16:12:21 -0500

    Fix dependency versions

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app
-from app.models import db, User, Predio
 import os
 
 app = create_app()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
