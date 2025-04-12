952758c2e61b0f9f40265d878aa1894d136be7ba
Yecsider <jyrios@ucundinamarca.edu.co>
2025-04-12 16:15:28 -0500

    Fix SQLAlchemy compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={"autoflush": False})
 login_manager = LoginManager()
 login_manager.login_view = 'routes.login'
 
