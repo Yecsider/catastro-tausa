69bd3088a42db67434444aa433b082b86e796b1a
Yecsider <jyrios@ucundinamarca.edu.co>
2025-04-13 16:57:33 -0500

    Fix dependency versions

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+from app.models import User 
+
 
 db = SQLAlchemy(session_options={"autoflush": False})
 login_manager = LoginManager()
@@ -18,3 +20,9 @@ def create_app(config_class=Config):
     app.register_blueprint(routes.bp)
     
     return app
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
