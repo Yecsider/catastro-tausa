9b3e186654e769a985b76f9ccf454f449b00bad0
Yecsider <jyrios@ucundinamarca.edu.co>
2025-04-13 14:57:11 -0500

    Fix dependency versions

--- app/models.py
+++ app/models.py
@@ -10,11 +10,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(256), nullable=False)
     isadmin = db.Column(db.Boolean, default=False)
     
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
     
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return f'<User {self.username}>'
